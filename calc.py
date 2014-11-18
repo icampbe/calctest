797681f291abe89e9a310c679a81d174cbc3f78d
Ian Campbell <icampbe@purdue.edu>
2014-11-18 12:39:12 -0500

    changed spaces to tabs

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-# Copyright 2014 -- Levi Starrett
+		# Copyright 2014 -- Levi Starrett
 # for educational purposes only
 #
 # Calculator -- a four function calculator commandline tool
@@ -13,25 +13,25 @@ import sys
 # a -- addend
 # b -- augend
 def add(a, b):
-    return a + b
+	return a + b
 
 # Subtract function
 # a -- minuend
 # b -- subtrahend
 def sub(a, b):
-    return a - b
+	return a - b
 
 # Multiply function
 # a -- multiplicand
 # b -- multiplier
 def mult(a, b):
-    return a * b
+	return a * b
 
 # Divide function
 # a -- dividend
 # b -- divisor
 def div(a, b):
-    return a / b
+	return a / b
 
 
 # -------------------------------------------------------- #
@@ -46,32 +46,33 @@ b = None
 op = None
 
 while (True):
-    # get input values
-    a = raw_input("Enter the first argument: ")
-    op = raw_input("Enter the operation: ")
-    b = raw_input("Enter the second argument: ")
-    try:
-        a = int(a)
-        b = int(b)
-    except ValueError:
-        print "Invalid number argument..."
-        op = None
+	# get input values
+	a = raw_input("Enter the first argument: ")
+	op = raw_input("Enter the operation: ")
+	b = raw_input("Enter the second argument: ")
+	try:
+		a = int(a)
+		b = int(b)
+	except ValueError:
+		print "Invalid number argument..."
+		op = None
 
-    # decide function
-    if (op != None):
-        if (op == "+"):
-            print "Sum: ", add(a, b)
-        elif (op == "-"):
-            print "Difference: ", sub(a, b)
-        elif (op == "*"):
-            print "Product: ", mult(a, b)
-        elif (op == "/"):
-            print "Quotient: ", div(a, b)
-        else:
-            print "Invalid operation..."
-
-    q = raw_input("Quit? [y/n] ")
-    if (q == "y" or q == "Y"):
-        break
+	# decide function
+	if (op != None):
+		if (op == "+"):
+			print "Sum: ", add(a, b)
+		elif (op == "-"):
+			print "Difference: ", sub(a, b)
+		elif (op == "*"):
+			print "Product: ", mult(a, b)
+		elif (op == "/"):
+			print "Quotient: ", div(a, b)
+		
+		else:
+			print "Invalid operation..."
+	
+	q = raw_input("Quit? [y/n] ")
+	if (q == "y" or q == "Y"):
+		break
 
 # -------------------------------------------------------- #
